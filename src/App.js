75a2077aa8d31d5cafcff92cb779f1f4e7244d85
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2021-12-23 20:43:28 +0200

    Implement dynamic split columns and movecolumns and data our of Table component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import useFetch from './hooks/useFetch'
 import useInterval from './hooks/useInterval'
 import Table from './components/Table/Table'
 import Header from './components/Header/Header'
+import { formatTime, getTotalTime } from './utils'
+import Data from './Data'
+
+const getColumns = (data) => {
+    return [
+        {
+            Header: 'Driver',
+            accessor: 'driver',
+            minWidth: 300,
+        },
+        {
+            Header: 'Number',
+            accessor: 'number',
+            width: 110,
+        },
+        ...data[0]?.splits.map((split, index) => ({
+            Header: `Split ${index + 1}`,
+            accessor: `splits[${index}]`,
+            Cell: ({ value }) => formatTime(value),
+            width: 110
+        })),
+        {
+            Header: 'Time',
+            accessor: 'splits',
+            Cell: ({ value }) => formatTime(getTotalTime(value)),
+            width: 110,
+        }
+    ]
+}
 
 const App = () => {
     const { data, url, setUrl, refresh } = useFetch('')
@@ -12,7 +41,7 @@ const App = () => {
         <>
             <Header url={url} setUrl={setUrl} />
             <main>
-                <Table data={data} />
+                <Table data={Data} columns={getColumns(Data)} />
             </main>
         </>
     )
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,16 +1,7 @@
 import './Table.css'
-import { useMemo } from 'react'
-import { COLUMNS } from './Columns'
 import { useTable, useSortBy, useFlexLayout  } from 'react-table'
-import Data from './Data'
 
-
-const Table = () => {
-
-    const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => Data, [])
-
-
+const Table = ({ data, columns }) => {
     const {
         getTableProps,
         getTableBodyProps,
@@ -28,43 +19,35 @@ const Table = () => {
 
     return (
         <div className='table-container'>
-            <table {...getTableProps() }>
+            <table {...getTableProps()}>
                 <thead>
-                    {
-                        headerGroups.map((headerGroups) => (
-
-                            <tr {...headerGroups.getHeaderGroupProps()}>
-                                {headerGroups.headers.map( (column) => (
-                                    <th   {...column.getHeaderProps(column.getSortByToggleProps())}
-                                        onClick={() => column.toggleSortBy(!column.isSortedDesc)} >
-                                        {column.render('Header')}</th>
-                                ))}
-                            </tr>
-
-                        ))
-                    }
-
+                    {headerGroups.map((headerGroups) => (
+                        <tr {...headerGroups.getHeaderGroupProps()}>
+                            {headerGroups.headers.map( (column) => (
+                                <th
+                                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                                    onClick={() => column.toggleSortBy(!column.isSortedDesc)}
+                                >
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {
-                        rows.map((row) => {
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {
-                                        row.cells.map( (cell) => {
-                                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                        })
-                                    }
-                                </tr>
-                            )
-                        })
-                    }
-
+                    {rows.map((row) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map( (cell) => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
-
     )
 }
 
