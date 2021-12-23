d669fc5809c23db5e14b6d6ede65680cf43f7d46
VicKLyo <gsbgvikistarbg@abv.bg>
2021-12-23 18:25:01 +0200

    Added time formatting & total time

--- src/components/Table/Columns.js
+++ src/components/Table/Columns.js
@@ -1,15 +1,50 @@
+import { formatTime, getTotalTime } from '../../utils'
+
 export const COLUMNS = [
     {
         Header: 'Driver',
-        accessor: 'driver'
+        accessor: 'driver',
+        minWidth: 300,
     },
     {
         Header: 'Number',
-        accessor: 'number'
+        accessor: 'number',
+        width: 110,
     },
     {
-        Header: 'Splits',
-        accessor: 'splits'
+        Header: 'Split 1',
+        accessor: 'splits[0]',
+        Cell: ({ value }) => formatTime(value),
+        width: 110,
+    },
+    {
+        Header: 'Split 2',
+        accessor: 'splits[1]',
+        Cell: ({ value }) => formatTime(value),
+        width: 110,
+    },
+    {
+        Header: 'Split 3',
+        accessor: 'splits[2]',
+        Cell: ({ value }) => formatTime(value),
+        width: 110,
+    },
+    {
+        Header: 'Split 4',
+        accessor: 'splits[3]',
+        Cell: ({ value }) => formatTime(value),
+        width: 110,
+    },
+    {
+        Header: 'Split 5',
+        accessor: 'splits[4]',
+        Cell: ({ value }) => formatTime(value),
+        width: 110,
+    },
+    {
+        Header: 'Time',
+        accessor: 'splits',
+        Cell: ({ value }) => formatTime(getTotalTime(value)),
+        width: 110,
     }
-
 ]
